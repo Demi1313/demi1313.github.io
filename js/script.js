458cdc7b32570cc007d4d0db64c86216e3575659
Dmitry Nikotiin <120470404+Demi1313@users.noreply.github.com>
2023-01-31 17:15:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+const percents = document.querySelectorAll('.skills__item__grid__percent'),
+lines = document.querySelectorAll('.skills__item__scale__load');
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
