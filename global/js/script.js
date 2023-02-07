b482f340da38a01a3ed752bdb9f505a1f75411ab
Dmitry Nikotiin <120470404+Demi1313@users.noreply.github.com>
2023-02-07 14:17:22 +0300

    Add files via upload

---
+++ global/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+      arrows: true, 
+      dots: false, 
+      slidesToShow: 3, 
+      centerMode: true,
+      centerPadding: '5px',
+      variableWidth: true
+  });
+
+});
+
